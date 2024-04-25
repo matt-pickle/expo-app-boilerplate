1b545f262718ff2388ef0d350844c3acd6059868
Matt <mpickle86@gmail.com>
2024-04-25 18:31:52 -0500

    Style Login screen

--- styles/LoginStyles.js
+++ styles/LoginStyles.js
@@ -1,8 +1,37 @@
 import { StyleSheet } from "react-native"
-import { mediumGray } from "./style-variables.js"
+import { lightGray } from "./style-variables.js"
 
 const styles = StyleSheet.create({
-
+  loginScreen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  loginScreenLogo: {
+    height: 300,
+    width: 300,
+    resizeMode: "contain",
+    marginBottom: 100
+  },
+  loginButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderColor: lightGray,
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 20
+  },
+  loginButtonText: {
+    color: lightGray,
+    fontSize: 16
+  },
+  googleIcon: {
+    color: lightGray,
+    fontSize: 24,
+    marginRight: 15
+  }
 })
 
 export default styles
